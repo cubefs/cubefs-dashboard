b258ad64f1fb736bcb248e88f07843db003c2d09
wangxiaodong1 <xiaodongwang202306@163.com>
2023-08-04 18:51:58 +0800

    feat(encrypt): add encrypt option
    Signed-off-by: wangxiaodong1 <xiaodongwang202306@163.com>

--- backend/main.go
+++ backend/main.go
@@ -16,18 +16,32 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/cubefs/cubefs-dashboard/backend/config"
 	"github.com/cubefs/cubefs-dashboard/backend/helper"
 	"github.com/cubefs/cubefs-dashboard/backend/model/migrate"
 	"github.com/cubefs/cubefs-dashboard/backend/model/mysql"
 	"github.com/cubefs/cubefs-dashboard/backend/router"
+	"github.com/cubefs/cubefs-dashboard/backend/helper/crypt"
 )
 
 var confPath = flag.String("c", "", "please input your conf file path")
+var enc = flag.String("e", "", "please input your encrypted string")
 
 func main() {
 	flag.Parse()
+	if *enc != "" {
+		str, err := crypt.Encrypt([]byte(*enc))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(str)
+		os.Exit(0)
+	}
+
 	helper.Must(config.Init(*confPath))
 	helper.Must(mysql.Init())
 	helper.Must(migrate.Init())
